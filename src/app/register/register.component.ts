3ae5d957f7b1657dadc2eaa964c76232f35c88ab
Tarek <severos@users.noreply.github.com>
2018-05-08 18:06:47 +0200

    Implement Http calls for Register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserRegisterModel, Gender } from '../models/user-register-model';
 import { NgForm, FormBuilder, Validators } from '@angular/forms';
+import { RemoteUrl } from '../models/remote-url';
+import { ServerResponse } from '../models/server-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,8 +19,9 @@ export class RegisterComponent implements OnInit {
   public valid_password: boolean;
   public valid_confirm_password: boolean;
   public valid_gender: boolean;
+  public errors: Array<{ code, description }>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.model = new UserRegisterModel();
@@ -26,13 +30,19 @@ export class RegisterComponent implements OnInit {
 
   public register(): void {
     console.log(this.model);
-    console.log(this.valid_name = this.model.ValidateName());
-    console.log(this.valid_email = this.model.ValidateEmail());
-    console.log(this.valid_gender = this.model.ValidateGender());
-    console.log(this.valid_password = this.model.ValidatePassword());
-    console.log(this.valid_confirm_password = this.model.ValidatePasswordConfirmation());
+    this.valid_name = this.model.ValidateName();
+    this.valid_email = this.model.ValidateEmail();
+    this.valid_gender = this.model.ValidateGender();
+    this.valid_password = this.model.ValidatePassword();
+    this.valid_confirm_password = this.model.ValidatePasswordConfirmation();
     if (this.valid_name && this.valid_email && this.valid_gender && this.valid_password && this.valid_confirm_password) {
-      console.log('valid');
+      this.http.post<ServerResponse<Array<{ code, description }>>>(RemoteUrl.Account.Register, this.model).subscribe((next) => {
+        if (next.success) {
+          this.router.navigate(['/home']);
+        } else {
+          this.errors = next.result;
+        }
+      });
     }
   }
 }
