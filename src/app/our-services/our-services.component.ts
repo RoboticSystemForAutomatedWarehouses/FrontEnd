03fef0d6cbe0c9ae587c150af049b4511006b9a6
Tarek <severos@users.noreply.github.com>
2018-06-16 07:21:48 +0200

    Add our services functionality.

--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RemoteUrl } from '../models/remote-url';
 import { ServerResponse } from '../models/server-response';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-our-services',
@@ -12,16 +13,19 @@ export class OurServicesComponent implements OnInit {
 
   public message: string;
   public data: Array<Warehouse>;
+  public order: Array<StorageSpace>;
+  public model: StorageSpace;
+  public editing: boolean;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public auth: AuthenticationService) {
     this.message = 'Loading... Please wait';
+    this.order = JSON.parse(localStorage.getItem('order')) || [];
   }
 
   ngOnInit() {
     this.http.get<ServerResponse<Array<Warehouse>>>(RemoteUrl.Warehouse).subscribe(res => {
       console.log(res);
       if (!res.success) {
-        console.error(res);
         this.message = 'Error while loading, please check developer console for more details, or try refreshing page.';
         return;
       }
@@ -32,10 +36,38 @@ export class OurServicesComponent implements OnInit {
     });
   }
 
-  addOrder(warehouse: Warehouse) {
-    console.log(warehouse);
+  private saveOrder() {
+    localStorage.setItem('order', JSON.stringify(this.order));
+    this.model = null;
   }
 
+  cancel() {
+    this.model = null;
+  }
+
+  createOrder(warehouse: Warehouse) {
+    this.editing = false;
+    this.model = new StorageSpace();
+    this.model.warehouseId = warehouse.id;
+  }
+
+  addOrder(storage: StorageSpace, add: boolean) {
+    if (add) {
+      this.order.push(storage);
+    }
+    this.saveOrder();
+    this.model = null;
+  }
+
+  editOrder(idx: number) {
+    this.editing = true;
+    this.model = this.order[idx];
+  }
+
+  removeOrder(idx: number) {
+    this.order.splice(idx, 1);
+    this.saveOrder();
+  }
 }
 
 class Warehouse {
@@ -48,4 +80,11 @@ class PriceSchema {
   public baseCost: number;
   public taxPercent: number;
   public dailyRate: number;
+}
+
+class StorageSpace {
+  public warehouseId: number;
+  public quantity: number;
+  public startDate: Date;
+  public endDate: Date;
 }
