d0b3e5081578d66928a03a502679f975ea90db3d
Tarek <severos@users.noreply.github.com>
2018-06-20 08:59:02 +0200

    implement persistent authentication

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,13 +3,28 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthenticationService {
 
-  constructor() { }
+  constructor() {
+    this.load();
+  }
 
   public get isAuthenticated(): boolean {
     return !!this.Id;
   }
 
-  public isAdmin: boolean;
-  public Id: string;
+  private _Id: string;
+  public get Id(): string {
+    return this._Id;
+  }
+  public set Id(v: string) {
+    this._Id = v;
+    this.save();
+  }
 
+  private save() {
+    localStorage.setItem('Id', this.Id);
+  }
+
+  private load() {
+    this._Id = localStorage.getItem('Id');
+  }
 }
