8889637fbea67d5c4382935ed6ce6ab0d273195d
Tarek <severos@users.noreply.github.com>
2018-06-21 14:17:14 +0200

    Add auto redirect on unauthenticated.

--- src/app/authentication-interceptor.ts
+++ src/app/authentication-interceptor.ts
@@ -1,31 +1,47 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { AuthenticationService } from './services/authentication.service';
 import { ServerResponse } from './models/server-response';
 import { RemoteUrl } from './models/remote-url';
+import { Router } from '@angular/router';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-    constructor(private service: AuthenticationService) { }
+    constructor(private service: AuthenticationService,
+        private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         req = req.clone({ withCredentials: true });
+        let handler: Observable<HttpEvent<any>>;
         if (req.url === RemoteUrl.Account.Logout || req.url === RemoteUrl.Account.Login) {
-            return next.handle(req).map(event => {
+            handler = next.handle(req).map(event => {
                 if (event instanceof HttpResponse && event.status === 200) {
                     const res = <ServerResponse<any>>event.body;
-                    console.log(event);
                     if (res.success) {
                         this.service.Id = res.message;
                     }
                 }
                 return event;
             });
+        } else {
+            handler = next.handle(req);
         }
-        return next.handle(req);
+        return handler.catch((err, caught) => {
+            if (err instanceof HttpErrorResponse && err.status === 401) {
+                this.handleNotAuthorized();
+                return;
+            }
+            return caught;
+        });
+    }
+
+    private handleNotAuthorized() {
+        alert('This page requires being authorized. You\'ll be redirected to login page momentarily.');
+        this.service.Id = null;
+        this.router.navigate(['/login']);
     }
 }
