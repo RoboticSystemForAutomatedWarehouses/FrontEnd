b780f687c49cb2f6f527b75e101fe5076ca2a3dd
Tarek <severos@users.noreply.github.com>
2018-06-23 18:23:11 +0200

    Fix MapIteratorPipe

--- src/app/pipes/map-iterator.pipe.ts
+++ src/app/pipes/map-iterator.pipe.ts
@@ -1,21 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'mapIterator'
+    name: 'mapIterator'
 })
 export class MapIteratorPipe implements PipeTransform {
 
-  transform(map: Map<any, any>): any[] {
-    const ret = [];
-
-    map.forEach((val, key) => {
-        ret.push({
-            key: key,
-            val: val
-        });
-    });
-    console.log(ret);
-    return ret;
-}
+    transform(map: Map<any, any>): Array<{ key: any, val: any }> {
+        const ret = [];
+        if (map) {
+            map.forEach((val, key) => {
+                ret.push({
+                    key: key,
+                    val: val
+                });
+            });
+        }
+        return ret;
+    }
 
 }
