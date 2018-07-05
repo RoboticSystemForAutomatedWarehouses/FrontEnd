ca9fd7aa95378267a3d0a24208894e65782cde1d
Tarek <severos@users.noreply.github.com>
2018-07-05 16:27:44 +0200

    change header background and animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { interval } from 'rxjs/observable/interval';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,29 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  messages: Array<string>;
+  public message: string;
+
   public displayImage: boolean;
-  constructor(public authenticationService: AuthenticationService, private route: ActivatedRoute, private router: Router) { }
+  constructor(public authenticationService: AuthenticationService, private route: ActivatedRoute, private router: Router) {
+    this.messages = [
+      'عاملين ايه على الغدا؟',
+      'كان فيه ماتش ايه اليوم؟',
+      'الarm اتكسرت ولا لسا؟',
+      'هنفطر ايه النهارده؟',
+      'هنتقابل ايمتى مع التيم؟',
+      'شهاب جاي ولا مش جاي؟',
+      'كيف بدأ الخلق؟',
+      'انا مين؟',
+      'شرف فين؟'
+    ];
+    this.message = this.messages[0];
+    let i = 0;
+    setInterval(() => {
+      this.message = this.messages[i];
+      i = (i + 1) % this.messages.length;
+    }, 3000);
+  }
 
   ngOnInit() {
     this.displayImage = true;
